6c6a1fd8d6b36cdcd8156c8bec72a32202e643d0
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-05-29 11:47:18 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<malloc.h>
 
 #include "arxivator.h"
 #include "dearxivator.h"
 
 
 int main(int argc, char* argv[]) {
-	FILE* in, * out;
 	if (argc == 1) {
 		printf("Not parameters\n");
 		return 0;
 	}
-	char komanda = argv[1];
-	in = fopen(argv[2], "rb");
-	out = fopen(argv[3], "wb");
-	if (komanda == 'a')
-		arxivation(in, out);
-	if (komanda == 'x') 
-		dearxivation(in, out);
-	fclose(in);
-	fclose(out);
+	if (argv[1] == 'a')
+		arxivation(argv[2], argv[3]);
+	if (argv[1] == 'x')
+		dearxivation(argv[2], argv[3]);
 	return 0;
 }
