40010dd31894d8a01b6997fc0c0e042db828db37
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-06-02 12:23:30 +0700

    Update dearxivator.c

--- dearxivator.c
+++ dearxivator.c
@@ -5,9 +5,12 @@
 #include<stdlib.h>
 #include<malloc.h>
 
+#include "st_node.h"
+
 void restore_tree(Xnode* h, FILE* in) {
 	char c;
 	fread(&c, sizeof(char), 1, in);
+	printf("%c ", c);
 	if (c == '0') {
 		Xnode* p = (Xnode*)malloc(sizeof(Xnode));
 		p->list = 0;
@@ -41,20 +44,29 @@ void restore_tree(Xnode* h, FILE* in) {
 char decoding(Xnode* h, FILE* in) {
 	if (h->list)
 		return h->data;
-	int bit_code;
-	fread(&bit_code, 1, 1, in);
+	char bit_code;
+	fread(&bit_code, sizeof(char), 1, in);
 	if (bit_code)
 		return decoding(h->right, in);
 	else
 		return decoding(h->left, in);
 }
+void printp(Xnode* t) {
+	if (!t) return;
+	printf("%c ", t->data);
+	printp(t->left);
+	printp(t->right);
+}
 
-void dearxivation(char* file_in, char* file_out){
+void dearxivation(char* file_in, char* file_out) {
 	FILE* in, * out;
 	in = fopen(file_in, "rb");
 	out = fopen(file_out, "wb");
 	Xnode* root = NULL;
 	restore_tree(root, in);
+	if (root)
+		printp(root);
+	else printf("1 ");
 	decoding(root, in);
 	fclose(in);
 	fclose(out);
