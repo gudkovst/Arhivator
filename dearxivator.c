0e5d8d2cc0a62e5a04e961d3438a1cc526a63f37
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-06-04 19:40:35 +0800

    Update dearxivator.c

--- dearxivator.c
+++ dearxivator.c
@@ -7,9 +7,10 @@
 
 #include "st_node.h"
 
-Xnode* create_node(char k, Xnode* l, Xnode* r) {
+long long COUNT;
+
+Xnode* create_node(Xnode* l, Xnode* r) {
 	Xnode* p = (Xnode*)malloc(sizeof(Xnode));
-	p->list = k;
 	p->left = l;
 	p->right = r;
 	return p;
@@ -21,49 +22,46 @@ Xnode* restore_tree(FILE* in) {
 	if (!c) {
 		Xnode* l = restore_tree(in);
 		Xnode* r = restore_tree(in);
-		Xnode* h = create_node(0, l, r);
-		
+		return create_node(l, r);
 	}
 	else {
-		Xnode* p = create_node(1, NULL, NULL);
+		Xnode* p = create_node(NULL, NULL);
 		fread(&p->data, sizeof(char), 1, in);
 		return p;
 	}
 }
 
 void decoding(Xnode* h, FILE* in, FILE* out) {
-	if (h->list) {
-		fwrite(&h->data, sizeof(char), 1, out);
-		return;
+	Xnode* p = h;
+	int k = 0;
+	char byte;
+	while (COUNT){
+		char bit;
+		fread(&byte, sizeof(char), 1, in);
+		bit = byte && (1 << (7 - k));
+		if (bit)
+			p = p->right;
+		else
+			p = p->left;
+		if (!p->left && !p->right){
+			fwrite(&p->data, sizeof(char), 1, out);
+			COUNT--;
+			p = h;
+		}
+		k = (k + 1) % 8;
+		if (!k)
+			fread(&byte, sizeof(char), 1, in);
 	}
-	char bit_code;
-	fread(&bit_code, sizeof(char), 1, in);
-	if (bit_code == 1)
-		decoding(h->right, in, out);
-	else
-		decoding(h->left, in, out);
-}
 
-void printp(Xnode* h) {
-	if (h->list) {
-		printf("%c", h->data);
-		return;
-	}
-	printf("0");
-	printp(h->left);
-	printf("1");
-	printp(h->right);
 }
 
 void dearxivation(char* file_in, char* file_out) {
 	FILE* in, * out;
 	in = fopen(file_in, "rb");
 	out = fopen(file_out, "wb");
-	int count;
-	fread(&count, sizeof(int), 1, in);
+	fread(&COUNT, sizeof(long long), 1, in);
 	Xnode* root = restore_tree(in);
-	for (int i = 0; i < count; i++)
-		decoding(root, in, out);
+	decoding(root, in, out);
 	fclose(in);
 	fclose(out);
 }
