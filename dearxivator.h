7c6f528af754eb24e1cc36d51b50923cef438647
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-06-04 19:41:08 +0800

    Update dearxivator.h

--- dearxivator.h
+++ dearxivator.h
@@ -4,7 +4,7 @@
 #include<stdio.h>
 #include "st_node.h"
 
-Xnode* create_node(char k, Xnode* l, Xnode* r);
+Xnode* create_node(Xnode* l, Xnode* r);
 void restore_tree(FILE* in);
 void decoding(Xnode* h, FILE* in, FILE* out);
 void dearxivation(char* file_in, char* file_out);
