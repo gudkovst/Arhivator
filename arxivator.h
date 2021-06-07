3ed7a690feee98bd4b883b996d3c0507145e4145
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-06-07 21:41:08 +0800

    Update arxivator.h

--- arxivator.h
+++ arxivator.h
@@ -5,12 +5,11 @@
 #include "st_node.h"
 
 void create();
-void initialization(char x);
+void initialization(unsigned char x);
 void swap(Xnode** a, Xnode** b);
 void sort();
-Xnode* make_tree(int N);
+Xnode* make_tree();
 void razdacha_kod(Xnode* h, char* sk, int k);
 void write_tree(Xnode* p, FILE* out);
 void print_kod(FILE* in, FILE* out);
 void arxivation(char* file_in, char* file_out);
-
