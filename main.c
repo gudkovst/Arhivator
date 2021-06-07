dc00a0c5def6488868dcb41d0437d767cf947759
gudkovst <69453922+gudkovst@users.noreply.github.com>
2021-06-07 21:39:59 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<malloc.h>
+#include<string.h>
 
 #include "arxivator.h"
 #include "dearxivator.h"
@@ -11,17 +13,27 @@ int main(int argc, char* argv[]) {
 		printf("Not parameters\n");
 		return 0;
 	}
-	if (argv[1][0] == 'a'){
+	char* file_in = (char*)malloc(150 * sizeof(char));
+	char* file_out = (char*)malloc(150 * sizeof(char));
+	int nin = strlen(argv[2]);
+	int nout = strlen(argv[3]);
+	for (int i = 0; i < nin; i++) 
+		file_in[i] = argv[2][i];
+	file_in[nin] = '\0';
+	for (int i = 0; i < nout; i++)
+		file_out[i] = argv[3][i];
+	file_out[nout] = '\0';
+	if (argv[1][0] == 'a') {
 		if (argc == 3)
-			arxivation(argv[2], "z.bin");
-		else	
-			arxivation(argv[2], argv[3]);
+			arxivation(file_in, "z.bin");
+		else
+			arxivation(file_in, file_out);
 	}
-	if (argv[1][0] == 'x'){
+	if (argv[1][0] == 'x') {
 		if (argc == 3)
 			printf("Few parameters\n");
-		else	
-			dearxivation(argv[2], argv[3]);
+		else
+			dearxivation(file_in, file_out);
 	}
 	return 0;
 }
